2176e2db33767e2b64bb351abea75274f3609ccf
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2022-11-03 11:05:44 +0000

    image carousel
    adding stylings and attributes to image carousel

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+
+$(function(){
+    $('.main-carousel').slick({
+      autoplay:true,
+      slideToShow: 1,
+      autoplaySpeed:1000,
+      arrows: true,
+      dots: true,
+      fade: true,
+      speed: 2000,
+      infinite: true,
+      loop: true
+    });
+  });
+              
