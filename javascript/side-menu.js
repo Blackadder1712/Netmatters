4c4bb31fd66fd671abe265b81bb4450cc786cfe0
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2022-11-07 13:28:17 +0000

    side nav
    added side nav bar for mobile view and styling it

---
+++ javascript/side-menu.js
@@ -0,0 +1,12 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mypanel").style.width = "260px";
+    document.getElementById("main").style.marginRight = "260px";
+    
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mypanel").style.width = "0";
+    document.getElementById("main").style.width = "0";
+  }
