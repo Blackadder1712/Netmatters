e601b22d66242e80ae182ce80b2132ea87bdaf8a
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2022-11-08 14:23:22 +0000

    nav and modal
    worked on nav and cookie modal

---
+++ javascript/cookie.js
@@ -0,0 +1,22 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+
+
+// Get the element that closes the modal
+var span = document.getElementById("cookie-1");
+var span2 = document.getElementById("cookie-2");
+
+
+
+
+//close on button click
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+span2.onclick = function() {
+    modal.style.display = "none";
+  }
+
+
