f22f2ac3402fa7fae25b8ddcdb1ac1d93abafd8b
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2022-11-09 12:15:37 +0000

    burger layout
    styled and adjusted hamburger layout to work on all vp sizes

---
+++ javascript/burger-2.js
@@ -0,0 +1,50 @@
+const hamMenu2 = document.querySelector(".hamburger-menu-2");
+
+
+
+hamMenu2.addEventListener("click",() =>
+{
+    if(
+    hamMenu2.classList.toggle("active"))
+    {
+        openNav();
+    }
+
+    else
+    {
+        closeNav();
+    }
+});
+
+function openNav() {
+    document.getElementById("mypanel").style.width = "260px";
+    document.getElementById("netmatters-work").style.marginRight = "260px";
+    document.getElementById("netmatters-work").style.marginLeft ="-260px";
+    document.getElementById("cards").style.marginRight = "270px";
+    document.getElementById("cards").style.marginLeft = "-270px";
+    document.getElementById("email-form").style.marginRight = "270px";
+    document.getElementById("email-form").style.marginLeft = "-270px";
+    document.getElementById("tail").style.marginRight = "270px";
+    document.getElementById("tail").style.marginLeft = "-260px";
+    document.getElementById("foot").style.marginRight = "270px";
+    document.getElementById("foot").style.marginLeft = "-260px";
+}
+   
+    function closeNav() {
+        document.getElementById("mypanel").style.width = "0";
+        document.getElementById("netmatters-work").style.marginLeft = "0";
+        document.getElementById("netmatters-work").style.marginRight = "0";
+        document.getElementById("cards").style.marginRight = "0";
+        document.getElementById("cards").style.marginLeft = "0";
+        document.getElementById("email-form").style.marginRight = "0";
+        document.getElementById("email-form").style.marginLeft = "0";
+        document.getElementById("tail").style.marginRight = "0";
+        document.getElementById("tail").style.marginLeft = "0";
+        document.getElementById("foot").style.marginRight = "0";
+        document.getElementById("foot").style.marginLeft = "0";
+      }
+   
+
+    
+  
+
---
+++ javascript/side-menu.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+    
+ 
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mypanel").style.width = "0";
+    document.getElementById("netmatters-work").style.marginLeft = "0";
+    document.getElementById("netmatters-work").style.marginRight = "0";
+    document.getElementById("cards").style.marginRight = "0";
+    document.getElementById("cards").style.marginLeft = "0";
+    document.getElementById("email-form").style.marginRight = "0";
+    document.getElementById("email-form").style.marginLeft = "0";
+    document.getElementById("tail").style.marginRight = "0";
+    document.getElementById("tail").style.marginLeft = "0";
+    document.getElementById("foot").style.marginRight = "0";
+    document.getElementById("foot").style.marginLeft = "0";
+  }
+
+
+    
