b5dc1d1f35617925c26f365dbd8406090c429691
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2022-11-13 13:12:51 +0000

    scrolling fixed nav
    scrolling fixed nav now only appears on scroll up

--- javascript/sticky.js
+++ javascript/sticky.js
@@ -1,4 +1,39 @@
+const body = document.body;
 
-    const container = document.getElementsByClassName('container-2');
-  
-    const result = container[0].getBoundingClientRect().top;
+
+
+let lastScroll = 0;
+
+window.addEventListener("scroll", () =>
+{
+    const currentScroll = window.pageYOffset //logs movement of page in pixels
+
+    if(currentScroll <= 0)
+    {
+        body.classList.remove("scroll-up"); //if at top of page remove scroll up css
+      
+     
+        
+        
+    }
+
+ 
+
+    if (currentScroll > lastScroll && !body.classList.contains("scroll-down"))
+    {
+       body.classList.remove("scroll-up")
+       body.classList.add("scroll-down")
+    } //comparing values and detecting scrolling down
+
+    if (currentScroll < lastScroll && body.classList.contains("scroll-down"))
+    {
+       body.classList.remove("scroll-down")
+       body.classList.add("scroll-up")
+    }
+
+   
+
+
+    
+    lastScroll = currentScroll;
+})
