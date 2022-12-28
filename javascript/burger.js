5e4bf2f71f8e54a1e8ac890c2315c6d6a7666b97
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2022-12-28 20:58:41 +0000

    overlay
    pverlay now appears when side menu is out

--- javascript/burger.js
+++ javascript/burger.js
@@ -53,15 +53,16 @@ function openNav() {
     document.getElementById("mypanel").style.width = "260px";
     document.getElementById("netmatters-work").style.marginRight = "260px";
     document.getElementById("netmatters-work").style.marginLeft ="-260px";
-    document.getElementById("cards").style.marginRight = "270px";
-    document.getElementById("cards").style.marginLeft = "-270px";
+
+   
     document.getElementById("email-form").style.marginRight = "270px";
     document.getElementById("email-form").style.marginLeft = "-270px";
     document.getElementById("tail").style.marginRight = "270px";
     document.getElementById("tail").style.marginLeft = "-260px";
     document.getElementById("foot").style.marginRight = "270px";
     document.getElementById("foot").style.marginLeft = "-260px";
-    document.getElementById("overlay").style.width = "100%";
+    document.getElementById("overlay").style.display = "block";
+
 
     
 
@@ -76,15 +77,16 @@ function openNav() {
         document.getElementById("mypanel").style.width = "0";
         document.getElementById("netmatters-work").style.marginLeft = "0";
         document.getElementById("netmatters-work").style.marginRight = "0";
-        document.getElementById("cards").style.marginRight = "0";
-        document.getElementById("cards").style.marginLeft = "0";
+       
+     
         document.getElementById("email-form").style.marginRight = "0";
         document.getElementById("email-form").style.marginLeft = "0";
         document.getElementById("tail").style.marginRight = "0";
         document.getElementById("tail").style.marginLeft = "0";
         document.getElementById("foot").style.marginRight = "0";
         document.getElementById("foot").style.marginLeft = "0";
-        document.getElementById("overlay").style.width = "0px";
+        document.getElementById("overlay").style.display = "none";
+ 
         
        
        
