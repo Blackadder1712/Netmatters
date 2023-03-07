108833ce7d751bf534815765acfe53f9a4a24eeb
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2023-03-07 13:10:51 +0000

    database
    database for news articles set up and filled

---
+++ Reusable/footer.php
@@ -0,0 +1,163 @@
+<div class="tail-box">
+      <div class="holder">
+        <footer class="tail" id="tail">
+
+          <div class="tail-inner">
+            <div class="about">
+              <h3>ABOUT NETMATTERS</h3>
+              <a href="#">
+                <h4> News </h4>
+              </a>
+              <a href="#">
+                <h4> Our Careers </h4>
+              </a>
+              <a href="#">
+                <h4> Our Team </h4>
+              </a>
+              <a href="#">
+                <h4> Privacy Policy </h4>
+              </a>
+              <a href="#">
+                <h4> Cookie Policy </h4>
+              </a>
+              <a href="#">
+                <h4> Terms & Conditions </h4>
+              </a>
+              <a href="#">
+                <h4> Environmental Policy </h4>
+              </a>
+              <a href="#">
+                <h4> Contact Us </h4>
+              </a>
+
+            </div>
+
+            <div class="services">
+
+              <h3>OUR SERVICES</h3>
+              <a href="#">
+                <h4> Bespoke Software </h4>
+              </a>
+              <a href="#">
+                <h4> IT Support </h4>
+              </a>
+              <a href="#">
+                <h4> Digital Marketing </h4>
+              </a>
+              <a href="#">
+                <h4> Telecoms Services</h4>
+              </a>
+              <a href="#">
+                <h4> Web Design </h4>
+              </a>
+              <a href="#">
+                <h4> Cyber Security </h4>
+              </a>
+              <a href="#">
+                <h4> Developer Training </h4>
+              </a>
+
+
+            </div>
+
+
+
+          </div>
+
+          <div class="offices">
+            <div class="cambridge">
+
+              <h3>CAMBRIDGE OFFICE</h3>
+              <h4> Unit 1.31, </h4>
+              <h4> St John's Innovation Centre,</h4>
+              <h4> Cowley Road,Milton, </h4>
+              <h4> Cambridge</h4>
+              <h4> CB4 0WS </h4>
+              <a href="tel:01223375772">
+                <h5> Tel: 01223 37 57 72 </h5>
+              </a>
+
+
+
+            </div>
+
+            <div class="wymondham">
+
+              <h3>WYMONDHAM OFFICE</h3>
+              <h4> Unit 15, </h4>
+              <h4> Penfold Drive,</h4>
+              <h4> Gateway 11 Business Park, </h4>
+              <h4> Wymondham,Norfolk,</h4>
+              <h4> NR18 0WZ </h4>
+              <a href="tel:01603704020">
+                <h5> Tel: 01603 70 40 20 </h5>
+              </a>
+
+
+
+            </div>
+
+            <div class="yarmouth">
+
+              <h3>GREAT YARMOUTH OFFICE</h3>
+              <h4> Suite F23, </h4>
+              <h4> Beacon Innovation Centre,</h4>
+              <h4> Beacon Park, Gorleston, </h4>
+              <h4> Great Yarmouth,Norfolk,</h4>
+              <h4> NR31 7RA </h4>
+              <a href="tel:01493603204">
+                <h5> Tel: 01493 60 32 04 </h5>
+              </a>
+            </div>
+
+
+
+
+          </div>
+
+
+
+
+
+
+
+        </footer>
+      </div>
+    </div>
+
+    <!--Copyright Information-->
+    <div class="shoe-box">
+      <div class="holder">
+        <div class="footer" id="foot">
+
+
+
+
+
+          <div class="copyright">
+
+            <h4><i class="fa-regular fa-copyright"></i>Copyright
+              Netmatters 2022. All rights reserved. -  <h5>Sitemap</h5></h4>
+
+          </div>
+
+
+          <div class="socials">
+
+            <i class="fa-brands fa-facebook" id="facebook"></i>
+            <i class="fa-brands fa-twitter" id="twitter"></i>
+            <i class="fa-brands fa-instagram" id="insta"></i>
+            <i class="fa-brands fa-linkedin-in" id="link"></i>
+
+
+
+
+
+
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+
