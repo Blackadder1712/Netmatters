ee49169370bb7dcaf9cbb7370d60ffec1c1669df
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2023-03-13 13:17:01 +0000

    styling
    styling contact page

--- contact.php
+++ contact.php
@@ -123,10 +123,23 @@
 
         </nav>
 
-        <div class= "Main-page-contact">
-
-          <div class = "contact-header">Our Offices</div>
+        <div class="contact-top">
+          <div class="nav-button">
+              <a href="#" class=" btn-map-home">Home</a>
+              <div class="home">VIEW MORE</div>
+          </div>
           
+          <div class= "Main-page-contact">
+            <div class = "contact-header">Our Offices</div>
+          </div>
+        </div>
+        
+    
+        
+          
+      
+          
+          <div class="map-deck">
             <?php
               
               foreach($maps as $map)
@@ -135,16 +148,20 @@
                           <img class="map-image" src='.$map["map_images"].' >
                           <div class="map-type">
                             <div class="map-body">
-                              <a href = "#"<h5 class="card-title-'.$map["map_class"].'">'.$map["map_title"]. '</h5></a>
-                              <p class="card-text" id="card-text">'.$map["map_info"].'
+                              <a href = "#" class ="map-title">'.$map["map_title"]. '</a>
+                              <p class="map-text" id="map-text">'.$map["map_info"].'
                             </p>
                               <a href="#" class="map-phone-'.$map["map_class"].'">'.$map["map_number"].'</a>
-                              <a href="#" class="btn btn-map-card">READ MORE</a>
-                              <div href="#" class="location">
-                              '.$map["location"].'
+                              <div class "btn-box">
+                               <a href="#" class="btn btn-map-card">VIEW MORE</a>
                               </div>
+                        
+                             
                               
                             
+                            </div>
+                            <div href="#" class="location">
+                            '.$map["location"].'
                             </div>
                           </div>
                         </div>  
@@ -156,6 +173,9 @@
 
                     
             ?>
+          </div>
+
+
 
                  
 
